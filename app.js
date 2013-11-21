4b0878fa82d7a46edc6803bd73a5fa6542975821
Kolin Krewinkel <kolin@kolinkrewinkel.com>
2013-11-20 21:13:37 -0800

    Good logic here

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ var express = require('express');
 var app = express();
 
 var Bitstamp = require('bitstamp');
-var bitstampClient = UNDEFINED;
+var bitstampClient = null;
 
 // Finances 
-var entryPrice = UNDEFINED;
+var orderRequired = true;
+var entryPrice = null;
+var lastPriceObserved = null;
 var positiveVarianceThreshold = 0.05;
 var negativeVarianceThreshold = 0.05;
 
@@ -35,7 +37,10 @@ function init()
 function intializeBitstampClient(key, secret, client_id)
 {
 	bitstampClient = new Bitstamp(key, secret, client_id);
-	enterAtMarket();
+
+	console.log("Initialized client with key: " + key + " secret: " + secret + " client ID: " + client_id);
+
+	beginRefreshingWithInterval(0.5);
 }
 
 function beginRefreshingWithInterval(seconds)
@@ -45,32 +50,60 @@ function beginRefreshingWithInterval(seconds)
 	}, seconds * 1000);
 }
 
-function tickerUpdated(results)
+function tickerUpdated(error, results)
 {
-	var currentUSDValue = results;
-	var minimumPriceToSell = entryPrice * (1.00 + positiveVarianceThreshold);
-	var maximumPriceToBuy = entryPrice * (1.00 - negativeVarianceThreshold);
+	var currentUSDValue = results["last"];
 
-	if (currentUSDValue >= minimumPriceToSell)
+	if (entryPrice == null && orderRequired)
 	{
-		sellAtMarket();
+		enterAtPrice(currentUSDValue);
 	}
-	else if (currentUSDValue <= maximumPriceToBuy)
+	else if (entryPrice != null)
 	{
-		enterAtMarket();shit
+		if (lastPriceObserved != null)
+		{
+			if (currentUSDValue === lastPriceObserved)
+			{
+				return;
+			}
+		}
+
+		var minimumPriceToSell = entryPrice * (1.00 + positiveVarianceThreshold);
+		var maximumPriceToBuy = entryPrice * (1.00 - negativeVarianceThreshold);
+
+		var change = currentUSDValue - entryPrice;
+		console.log("     Entry: $" + entryPrice + "\n   Current: $" + currentUSDValue + "\n    Change: $" + change + "\nChange (%): " + (change/entryPrice) * 100 + "%\n");
+
+		// if (currentUSDValue >= minimumPriceToSell)
+		// {
+		// 	sellAtMarket();
+		// }
+		// else if (currentUSDValue <= maximumPriceToBuy)
+		// {
+		// 	enterAtMarket();
+		// }
+
+		lastPriceObserved = currentUSDValue;
 	}
 }
 
-function enterAtMarket()
+function enterAtPrice(price)
 {
-	bitstampClient.buy(1, null, marketEntered);
+	orderRequired = false;
+	// bitstampClient.buy(1, price, marketEntered);
+
+	marketEntered(null, {"price": price});
 }
 
-function marketEntered(result)
+function marketEntered(error, result)
 {
-	entryPrice = result;
+	entryPrice = result["price"];
+	orderRequired = false;
+}
 
-	beginRefreshingWithInterval(0.5);
+function sellAtMarket()
+{
+
 }
 
 app.listen(5001);
